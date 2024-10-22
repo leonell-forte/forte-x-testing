e9ae4f0bb71b857e33f0c7f7a3941c6a36a4f89a
lcruzforte1107 <lcruz@forteglobal.com>
2024-10-22 14:03:42 +0800

    create button component

---
+++ src/app/components/page.tsx
@@ -0,0 +1,14 @@
+import Button from "@/components/ui/button";
+import React from "react";
+
+const ComponentsPage = () => {
+  return (
+    <div className="p-10 bg-forest-green">
+      <Button buttonType="tertiary" active>
+        Test
+      </Button>
+    </div>
+  );
+};
+
+export default ComponentsPage;
