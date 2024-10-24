a2e08a4458e5009fe8b8b023fa19596150a8c2d0
Teamified <teamified@Leonell-Cruz.local>
2024-10-25 07:12:39 +0800

    create dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,17 @@
 import Header from "@/components/Layout/Header/Header";
+import SidePanel from "@/components/Layout/SidePanel/SidePanel";
 import React, { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
+    <div className="space-y-5 min-h-screen">
       <Header />
-      {children}
+      <div className="px-5 flex gap-4 h-[88vh]">
+        <SidePanel />
+        <div className="w-full bg-white bg-opacity-[30%] rounded-[10px]">
+          {children}
+        </div>
+      </div>
     </div>
   );
 };
