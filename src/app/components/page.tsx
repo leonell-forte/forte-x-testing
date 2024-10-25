6ff41bbcf3e0219c60cf545763101c256ab973f6
Teamified <teamified@Leonell-Cruz.local>
2024-10-25 10:41:55 +0800

    customize checkbox

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Buttons from "./Buttons";
 import InputFields from "./InputFields";
+import Checkboxes from "./Checkboxes";
 
 const ComponentsPage = () => {
   return (
     <div className="p-10 space-y-10">
       <Buttons />
       <InputFields />
+      <Checkboxes />
     </div>
   );
 };
