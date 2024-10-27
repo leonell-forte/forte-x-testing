574de2a21a39ba0c6e373add17b09141bc8147cd
Teamified <teamified@Leonell-Cruz.local>
2024-10-28 07:15:11 +0800

    create menu dropdown for logout

---
+++ src/app/(dashboard)/beneficiaries/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BeneficiariesPage = () => {
+  return <div></div>;
+};
+
+export default BeneficiariesPage;
--- src/components/Layout/Header/UserDropdown.tsx
+++ src/components/Layout/Header/UserDropdown.tsx
@@ -1,23 +1,74 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { useOutsideClick } from "@/lib/hooks";
 
 const UserDropdown = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const router = useRouter();
+
+  const handleLogout = () => {
+    router.push("/login");
+  };
+
+  const dropdownRef = useRef(null);
+
+  useOutsideClick(dropdownRef, () => setShowDropdown(false));
+
   return (
-    <div className="w-[184px] h-[42px] rounded-[50px] bg-white bg-opacity-[30%] flex justify-between items-center pl-1.5 pr-4">
-      <div className="flex gap-1.5">
-        <div className="w-[28px] h-[28px] rounded-full bg-[#D9D9D9]"></div>
-        <div className="flex items-center px-2">
-          <p className="text-forest-green font-medium">Leonell</p>
+    <div ref={dropdownRef} className="relative">
+      <button
+        onClick={() => setShowDropdown((prev) => !prev)}
+        className="w-[184px] h-[42px] rounded-[50px] bg-white bg-opacity-[30%] flex justify-between items-center pl-1.5 pr-4"
+      >
+        <div className="flex gap-1.5">
+          <div className="w-[28px] h-[28px] rounded-full bg-[#D9D9D9]"></div>
+          <div className="flex items-center px-2">
+            <p className="text-forest-green font-medium">Leonell</p>
+          </div>
         </div>
-      </div>
-      <button className="px-1.5">
-        <Image
-          width={12}
-          height={12}
-          alt="arrow"
-          src="/images/icons/arrow.svg"
-        />
+        <button className="px-1.5">
+          <Image
+            width={12}
+            height={12}
+            alt="arrow"
+            src="/images/icons/arrow.svg"
+          />
+        </button>
       </button>
+      <motion.ul
+        initial={{ height: 0 }}
+        animate={
+          showDropdown
+            ? { opacity: 1, height: "fit-content" }
+            : { height: 0, opacity: 0 }
+        }
+        transition={{ type: "spring", duration: 0.4, bounce: 0 }}
+        className="absolute top-12 left-0 rounded-[4px] bg-white w-full overflow-hidden"
+      >
+        <Link href="">
+          <li className="text-black py-1.5 px-2.5 hover:bg-grey transition-all">
+            Profile
+          </li>
+        </Link>
+        <button
+          type="button"
+          onClick={handleLogout}
+          className="w-full text-left"
+        >
+          <li
+            onClick={handleLogout}
+            className="text-black py-1.5 px-2.5 hover:bg-grey transition-all"
+          >
+            Log out
+          </li>
+        </button>
+      </motion.ul>
     </div>
   );
 };
