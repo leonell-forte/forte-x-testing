4b4a7953eb6a1e621431959fe491d59d0e3d88d1
Teamified <teamified@Leonell-Cruz.local>
2024-10-28 07:28:30 +0800

    fix hydration error

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,7 +1,26 @@
 import { useDispatch, useSelector, useStore } from "react-redux";
 import type { RootState, AppDispatch, AppStore } from "./store";
+import { MutableRefObject, useEffect } from "react";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
 export const useAppStore = useStore.withTypes<AppStore>();
+
+export const useOutsideClick = (
+  ref: MutableRefObject<HTMLElement | null>,
+  callBack: () => void
+) => {
+  const handleClick = (e: MouseEvent) => {
+    if (ref.current && !ref.current.contains(e.target as Node)) {
+      callBack();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, []);
+
+  return null;
+};
