687ad2a8093cd60c6da63775899246a8cbb1d51c
Teamified <teamified@Leonell-Cruz.local>
2024-10-28 08:08:18 +0800

    fix build error

--- src/components/Layout/Header/UserDropdown.tsx
+++ src/components/Layout/Header/UserDropdown.tsx
@@ -22,7 +22,7 @@ const UserDropdown = () => {
 
   return (
     <div ref={dropdownRef} className="relative">
-      <div
+      <button
         onClick={() => setShowDropdown((prev) => !prev)}
         className="w-[184px] h-[42px] rounded-[50px] bg-white bg-opacity-[30%] flex justify-between items-center pl-1.5 pr-4"
       >
@@ -32,15 +32,15 @@ const UserDropdown = () => {
             <p className="text-forest-green font-medium">Leonell</p>
           </div>
         </div>
-        <button className="px-1.5">
+        <div className="px-1.5 cursor-pointer">
           <Image
             width={12}
             height={12}
             alt="arrow"
             src="/images/icons/arrow.svg"
           />
-        </button>
-      </div>
+        </div>
+      </button>
       <motion.ul
         initial={{ height: 0 }}
         animate={
@@ -61,10 +61,7 @@ const UserDropdown = () => {
           onClick={handleLogout}
           className="w-full text-left"
         >
-          <li
-            onClick={handleLogout}
-            className="text-black py-1.5 px-2.5 hover:bg-grey transition-all"
-          >
+          <li className="text-black py-1.5 px-2.5 hover:bg-grey transition-all">
             Log out
           </li>
         </button>
