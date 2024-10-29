f7f1c9978e7143023e4737ab2cb3dd4f3c0f5b45
Teamified <teamified@Leonell-Cruz.local>
2024-10-29 14:37:47 +0800

    remove export path map

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   distDir: "out",
   images: {
-    unoptimized: true, // Disable image optimization for static export
-  },
-  exportPathMap: async function (defaultPathMap) {
-    return {
-      "/": { page: "/" },
-      "/login.html": { page: "/login" },
-      "/signup.html": { page: "/signup" },
-      "/users.html": { page: "/users" },
-
-      // Define other custom routes here if needed
-    };
+    unoptimized: true, // Disable image optimization
   },
 };
 
