1f4971fd4c98f0588d3cda56d85e52799c7396df
Teamified <teamified@Leonell-Cruz.local>
2024-10-29 14:33:53 +0800

    add export path map

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,17 @@ const nextConfig: NextConfig = {
   output: "export",
   distDir: "out",
   images: {
-    unoptimized: true, // Disable image optimization
+    unoptimized: true, // Disable image optimization for static export
+  },
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      "/": { page: "/" },
+      "/login.html": { page: "/login" },
+      "/signup.html": { page: "/signup" },
+      "/users.html": { page: "/users" },
+
+      // Define other custom routes here if needed
+    };
   },
 };
 
