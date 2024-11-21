8e0e332c7e974efdc0e6cd86aba984b4cfb170d7
Teamified <teamified@Leonell-Cruz.local>
2024-11-21 14:32:09 +0800

    fix login screen styles and spacing

--- src/components/Login/OTPForm.tsx
+++ src/components/Login/OTPForm.tsx
@@ -22,12 +22,12 @@ const OTPForm = ({ handleNext }: ILoginProps) => {
   return (
     <div>
       <div className="text-center">
-        <p className="text-[24px] md:text-[32px]">Enter confirmation code</p>
+        <p className="text-[24px] md:text-[32px]">Enter code</p>
 
         <div className="text-[14px] md:text-[18px]">
-          <p>We set a code at </p>
+          <p>A code has been sent to </p>
 
-          <p>{email}</p>
+          <p>{email}.</p>
         </div>
       </div>
 
@@ -42,11 +42,11 @@ const OTPForm = ({ handleNext }: ILoginProps) => {
         disabled={!isComplete}
         fullWidth
       >
-        Continue
+        Log in
       </Button>
 
       <p className="text-center text-[14px] mt-[15px]">
-        Didn’t receive the email?{" "}
+        Didn&apos;t receive the email?{" "}
         <button className="font-bold">Resend code</button>
       </p>
     </div>
