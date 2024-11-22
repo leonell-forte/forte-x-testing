bd3933f1371c8ed5b0d37227d2750f1d3881732b
Teamified <teamified@Leonell-Cruz.local>
2024-11-22 14:11:48 +0800

    capitalize selected item on filter

--- src/pages/Organizations/OrganizationsPage.tsx
+++ src/pages/Organizations/OrganizationsPage.tsx
@@ -12,7 +12,6 @@ import OrganizationDialogue from "../../components/Dashboard/Organizations/Dialo
 import { REGIONS, STATUS, TYPES } from "../../lib/constants";
 import { useDebounce } from "../../lib/hooks";
 import closeFilter from "../../assets/images/icons/close-filter.svg";
-import { capitalize } from "@mui/material";
 
 const OrganizationsPage = () => {
   const [modal, setModal] = useState<"org" | null>(null);
@@ -129,7 +128,7 @@ const OrganizationsPage = () => {
 
           <Dropdown
             noHelperText
-            value={capitalize(filters.status)}
+            value={filters.status}
             handleSelect={(val) => {
               handleSelectFilter("status", val);
             }}
@@ -140,7 +139,7 @@ const OrganizationsPage = () => {
 
           <Dropdown
             noHelperText
-            value={capitalize(filters.type)}
+            value={filters.type}
             handleSelect={(val) => {
               handleSelectFilter("type", val);
             }}
