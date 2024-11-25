a0ef9c40578b28f394f1f45122225a1a60bbb063
Teamified <teamified@Leonell-Cruz.local>
2024-11-25 09:35:01 +0800

    fix login error message

--- src/lib/validators/auth.ts
+++ src/lib/validators/auth.ts
@@ -10,18 +10,15 @@ export const login = {
   },
 
   schema: z.object({
-    email: z.string().email(),
+    email: z.string().email("Incorrect email or password"),
 
     password: z
       .string()
-      .min(8, "Password must be at least 8 characters long")
-      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
-      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-      .regex(/\d/, "Password must contain at least one number")
-      .regex(
-        /[!@#$%^&*(),.?":{}|<>]/,
-        "Password must contain at least one special character",
-      ),
+      .min(8, "Incorrect email or password")
+      .regex(/[a-z]/, "Incorrect email or password")
+      .regex(/[A-Z]/, "Incorrect email or password")
+      .regex(/\d/, "Incorrect email or password")
+      .regex(/[!@#$%^&*(),.?":{}|<>]/, "Incorrect email or password"),
     remember: z.boolean(),
   }),
 };
