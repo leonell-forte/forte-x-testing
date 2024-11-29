6e0bea87a2fc87d66f41c99f3e26e251abe3aa19
Teamified <teamified@Leonell-Cruz.local>
2024-11-29 10:48:58 +0800

    create horizontal scroller

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import auth from "./slice/auth";
 import alert from "./slice/alert";
+import scroll from "./slice/scroll";
 import layout from "./slice/layout";
 
 export const makeStore = () => {
   return configureStore({
-    reducer: { auth, alert, layout },
+    reducer: { auth, alert, layout, scroll },
   });
 };
 
