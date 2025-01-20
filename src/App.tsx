fe4008a3a53f92072f9ad7074e0f66751a46041c
leonell-forte <lcruz@forteglobal.com>
2025-01-20 14:33:55 +0800

    wrap routes into a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { Suspense } from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import RouteProvider from "components/RouteProvider";
 
 import "./App.css";
-import AlertProvider from "./components/AlertProvider";
-import DashboardLayout from "./components/Dashboard/Layout";
-import Providers from "./components/Providers";
 import QueryProvider from "./components/QueryProvider";
-import Spinner from "./components/ui/spinner/spinner";
-import { PROTECTED_ROUTES, PUBLIC_ROUTES } from "./lib/routes";
 
 // Lazy load components
 
@@ -16,44 +10,7 @@ function App() {
     <QueryProvider>
       <div>
         <div className="fixed left-0 top-0 z-[-1] h-screen w-screen bg-body-gradient"></div>
-        <Router>
-          <Providers>
-            <AlertProvider>
-              <Suspense
-                fallback={
-                  <div className="flex h-full w-full items-center justify-center">
-                    <Spinner />
-                  </div>
-                }
-              >
-                <Routes>
-                  {PUBLIC_ROUTES.map((item, index) => {
-                    const { link, Component } = item;
-                    return (
-                      <Route key={index} path={link} element={<Component />} />
-                    );
-                  })}
-
-                  {PROTECTED_ROUTES.map((item, index) => {
-                    const { link, Component, restrictedRoles } = item;
-
-                    return (
-                      <Route
-                        key={index}
-                        path={link}
-                        element={
-                          <DashboardLayout restrictedRoles={restrictedRoles}>
-                            <Component />
-                          </DashboardLayout>
-                        }
-                      />
-                    );
-                  })}
-                </Routes>
-              </Suspense>
-            </AlertProvider>
-          </Providers>
-        </Router>
+        <RouteProvider />
       </div>
     </QueryProvider>
   );
