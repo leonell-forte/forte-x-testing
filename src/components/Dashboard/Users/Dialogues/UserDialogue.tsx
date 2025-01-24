466f854802619c40d8942d4673ab22985415da36
leonell-forte <lcruz@forteglobal.com>
2025-01-24 09:48:11 +0800

    fix phone number validator

--- src/components/Dashboard/Users/Dialogues/UserDialogue.tsx
+++ src/components/Dashboard/Users/Dialogues/UserDialogue.tsx
@@ -6,7 +6,6 @@ import { Controller, useForm } from "react-hook-form";
 
 import { ROLES } from "lib/constants";
 import useUserMutation from "lib/mutations/users";
-import { PHONE_NUMBER } from "lib/regex";
 import { IOrganization } from "lib/types/organizations";
 import { UserFieldTypes } from "lib/types/users";
 import { users } from "lib/validators/users";
@@ -181,7 +180,7 @@ const UserDialogue = ({
                 return (
                   <Input
                     {...field}
-                    pattern={PHONE_NUMBER}
+                    phoneNUmber
                     error={!!error?.message}
                     helperText={error?.message}
                     autoComplete="tel"
