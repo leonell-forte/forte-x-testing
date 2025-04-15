12bb5546311b258d7d2351a6797ca856723b3083
Echo Aldemo <jaldemo@forteglobal.com>
2025-04-16 07:22:28 +0800

    fix beneficiary page error

--- src/lib/isPhoneValid.ts
+++ src/lib/isPhoneValid.ts
@@ -3,6 +3,7 @@ import { PhoneNumberUtil } from "google-libphonenumber";
 const phoneUtil = PhoneNumberUtil.getInstance();
 
 export const isPhoneValid = (phone: string) => {
+  if (!phone) return false;
   try {
     return phoneUtil.isValidNumber(phoneUtil.parseAndKeepRawInput(phone));
   } catch (error) {
