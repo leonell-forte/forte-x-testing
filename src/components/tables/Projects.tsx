9610d08e06bf5635364d17a418c31092141c052a
leonell-forte <lcruz@forteglobal.com>
2025-04-30 09:46:25 +0800

    fix: align mobile cards with the table for projects list

--- src/components/tables/Projects.tsx
+++ src/components/tables/Projects.tsx
@@ -59,7 +59,15 @@ const ProjectsTable = ({
       <div className="lg:hidden">
         <Cards.Container isLoading={isLoading}>
           {list.map((item, index) => {
-            const { id, name, outcomes, providers } = item;
+            const {
+              id,
+              name,
+              funder,
+              budget,
+              contractsCount,
+              beneficiariesCount,
+              milestonesCount,
+            } = item;
 
             return (
               <Cards.Card
@@ -71,14 +79,21 @@ const ProjectsTable = ({
                 key={index}
                 title={name}
               >
-                <Cards.Group>
+                <Cards.Group cols={2}>
+                  <Cards.Details label="Funder" value={funder?.name || "-"} />
+                  <Cards.Details label="Budget" value={`$ ${budget}`} />
+
                   <Cards.Details
-                    label="Providers"
-                    value={providers?.map((item) => item).join(", ") || "-"}
+                    label="# of Contracts"
+                    value={contractsCount?.toString() || "0"}
                   />
                   <Cards.Details
-                    label="Outcomes"
-                    value={outcomes?.map((item) => item.name).join(", ")}
+                    label="# of Beneficiaries"
+                    value={beneficiariesCount?.toString() || "0"}
+                  />
+                  <Cards.Details
+                    label="# of Milestones"
+                    value={milestonesCount?.toString() || "0"}
                   />
                 </Cards.Group>
                 <div className="absolute bottom-3 right-3 flex gap-2">
@@ -88,9 +103,10 @@ const ProjectsTable = ({
                       id={id.toString()}
                       buttonType="default"
                       type="button"
-                      onClick={() =>
-                        showProjectDialogue({ projectId: item.id.toString() })
-                      }
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        showProjectDialogue({ projectId: item.id.toString() });
+                      }}
                       className="icon group"
                     >
                       <Pencil className="h-auto w-5 transition-all group-hover:fill-mint" />
@@ -103,7 +119,8 @@ const ProjectsTable = ({
                       id={id.toString()}
                       buttonType="default"
                       type="button"
-                      onClick={() => {
+                      onClick={(e) => {
+                        e.stopPropagation();
                         setModal("delete");
                         setSelectedProject(item);
                       }}
