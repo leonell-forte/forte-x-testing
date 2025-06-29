1f34737b939b51dfaf01642378da9995db26482b
leonell-forte <lcruz@forteglobal.com>
2025-06-30 06:42:44 +0800

    feat:create projectss homepage

--- apps/forte-x/src/App.tsx
+++ apps/forte-x/src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "./components/providers/theme-provider";
 import LoginPage from "./pages/public/LoginPage";
+import ProjectsPage from "./pages/public/ProjectsPage";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <LoginPage />
+      {/* <LoginPage /> */}
+      <ProjectsPage />
     </ThemeProvider>
   );
 }
---
+++ apps/forte-x/src/pages/public/ProjectsPage.tsx
@@ -0,0 +1,9 @@
+const ProjectsPage = () => {
+  return (
+    <div>
+      <h2>Projects</h2>
+    </div>
+  );
+};
+
+export default ProjectsPage;
