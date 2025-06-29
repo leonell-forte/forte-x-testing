3cef4dfe6c100e5039a235e27fcfe3702eff7bf6
leonell-forte <lcruz@forteglobal.com>
2025-06-30 07:04:18 +0800

    feat: create tabs component

--- apps/forte-x/src/pages/public/ProjectsPage.tsx
+++ apps/forte-x/src/pages/public/ProjectsPage.tsx
@@ -1,7 +1,27 @@
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../../../@repo/ui/components/tabs";
+
 const ProjectsPage = () => {
   return (
-    <div>
-      <h2>Projects</h2>
+    <div className="space-y-6 p-10">
+      <h2 className="text-2xl font-bold">Projects</h2>
+      <div className="flex items-center gap-4">
+        <p>Filter By</p>
+        <Tabs defaultValue="current" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="current">Current</TabsTrigger>
+            <TabsTrigger value="past">Past Projects</TabsTrigger>
+            <TabsTrigger value="all">All</TabsTrigger>
+          </TabsList>
+          {/* <TabsContent value="current"></TabsContent>
+          <TabsContent value="past"></TabsContent>
+          <TabsContent value="all"></TabsContent> */}
+        </Tabs>
+      </div>
     </div>
   );
 };
