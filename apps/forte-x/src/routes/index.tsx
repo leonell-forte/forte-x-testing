8557ce6a40f84f0c0f28f06d4a4b38f2821b651b
leonell-forte <lcruz@forteglobal.com>
2025-07-02 08:48:27 +0800

    feat: create routes for projects and students

--- apps/forte-x/src/routes/index.tsx
+++ apps/forte-x/src/routes/index.tsx
@@ -3,4 +3,8 @@ import LoginPage from "@/pages/public/LoginPage";
 
 export const publicRoutes = [{ path: "/login", element: <LoginPage /> }];
 
-export const privateRoutes = [{ path: "/projects", element: <ProjectsPage /> }];
+export const privateRoutes = [
+  { path: "/projects", element: <ProjectsPage /> },
+  { path: "/projects/:id", element: "project details" },
+  { path: "/student/:id", element: "student details" },
+];
